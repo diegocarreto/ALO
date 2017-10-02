892a4665277a61288ffb13ca8d2638ae70150d1e
Diego <diego.ao.carreto@gmail.com>
2017-10-01 23:38:00 -0500

    Envio de mails y cambios en cotizador

---
+++ Enviar Mail/enviarmail/enviarmail/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enviarmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           //Creamos un nuevo Objeto de mensaje
+            System.Net.Mail.MailMessage mmsg = new System.Net.Mail.MailMessage();
+
+            //Direccion de correo electronico a la que queremos enviar el mensaje
+            mmsg.To.Add("eg@aloalo.mx,la@aloalo.mx,eo@aloalo.mx,diego.ao.carreto@gmail.com");
+
+            //Nota: La propiedad To es una colección que permite enviar el mensaje a más de un destinatario
+
+            //Asunto
+            mmsg.Subject = "Cotización";
+            mmsg.SubjectEncoding = System.Text.Encoding.UTF8;
+
+            //Direccion de correo electronico que queremos que reciba una copia del mensaje
+            //mmsg.Bcc.Add("diego.ao.carreto@gmail.com"); //Opcional
+
+            string path = Directory.GetCurrentDirectory() + "\\email.html";
+
+            //Cuerpo del Mensaje
+            mmsg.Body = File.ReadAllText(path, Encoding.UTF8);
+            mmsg.BodyEncoding = System.Text.Encoding.UTF8;
+            mmsg.IsBodyHtml = true; //Si no queremos que se envíe como HTML
+
+
+            //Correo electronico desde la que enviamos el mensaje
+            mmsg.From = new System.Net.Mail.MailAddress("cotizaciones@alopruebas.com.mx");
+
+            mmsg.Attachments.Add(new Attachment(Directory.GetCurrentDirectory() + "\\Cotizaciones.xlsx"));
+
+            /*-------------------------CLIENTE DE CORREO----------------------*/
+
+            //Creamos un objeto de cliente de correo
+            System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient();
+
+            //Hay que crear las credenciales del correo emisor
+            cliente.Credentials =
+                new System.Net.NetworkCredential("cotizaciones@alopruebas.com.mx", "Cbcl04?1");
+
+            //Lo siguiente es obligatorio si enviamos el mensaje desde Gmail
+            
+            cliente.Port = 587;
+            //cliente.EnableSsl = true;
+            
+
+            cliente.Host = "mail.alopruebas.com.mx"; //Para Gmail "smtp.gmail.com";
+
+
+            /*-------------------------ENVIO DE CORREO----------------------*/
+
+            try
+            {
+                //Enviamos el mensaje      
+                cliente.Send(mmsg);
+            }
+            catch (System.Net.Mail.SmtpException ex)
+            {
+                //Aquí gestionamos los errores al intentar enviar el correo
+            }
+        }
+    }
+}
